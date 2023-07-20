3c25b27f1a75b869f4fc08bd760bbe5e79db98ff
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-20 17:05:33 +0200

    Update simple-image-processing-tool.py

--- simple-image-processing-tool.py
+++ simple-image-processing-tool.py
@@ -22,3 +22,4 @@ def apply_filter(img, filter_type):
         print("Error: Unable to apply filter.")
         print(str(e))  # Print the actual error message for debugging purposes
     return None
+
