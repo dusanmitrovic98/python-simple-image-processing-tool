e52400d2d69e2c0f580eaefb316963b0716c4f6d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-20 17:03:48 +0200

    Update simple-image-processing-tool.py

--- simple-image-processing-tool.py
+++ simple-image-processing-tool.py
@@ -18,3 +18,4 @@ def apply_filter(img, filter_type):
         return filtered_img
     except AttributeError:
         print(f"Error: Invalid filter type '{filter_type}'.")
+    except Exception as e:
